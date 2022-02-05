ffd4a0b45be725340d3c5b2d4ca7b14f9821acb4
ElieMirza <elie.mirza1987@gmail.com>
2022-02-05 18:34:39 +0200

    gender and age working, nationality needs more work

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,47 @@
 let user_input = "";
-let gender = "https://api.genderize.io?name=";
-let age = "https://api.agify.io/?name=";
-let nationality = "https://api.nationalize.io/?name=";
+let gender_api = "https://api.genderize.io?name=";
+let age_api = "https://api.agify.io/?name=";
+let nationality_api = "https://api.nationalize.io/?name=";
 
 fetchImage();
 
 document.getElementById("button").onclick = function(){
   user_input = document.getElementById("input").value;
-  // fetchJSON(gender, user_input)
-  fetchJSON(age, user_input)
-  // fetchJSON(nationality, user_input)
+  fetchGender(gender_api, user_input);
+  fetchAge(age_api, user_input);
+  fetchNationality(nationality_api, user_input);
 }
 
-function fetchJSON(link, userName) {
-  fetch(link + userName)
-    .then(response => response.json())
-    .then(data => alert(data.gender));
+async function fetchGender(link, userName) {
+    await fetch(link + userName)
+            .then(response => response.json())
+            .then(data => document.getElementById("gender").textContent = `gender: ${data["gender"]}`)
+}
+
+async function fetchAge(link, userName) {
+    await fetch(link + userName)
+            .then(response => response.json())
+            .then(data => document.getElementById("age").textContent = `age: ${data["age"]} years old`)
+}
+
+async function fetchNationality(link, userName) {
+    await fetch(link + userName)
+            .then(response => response.json())
+            .then(data => {countries = data.country});
+    console.log(countries);
+    let gender_para = document.getElementById("nationality");
+
+    countries.map(country => {
+      let span = document.createElement('span');
+      console.log(span);
+      span.textContent = country["country_id"];
+      console.log(span);
+      gender_para.appendChild(span);
+    });
+
+       // 'country["country_id"].value'
+    // .textContent = `nationality: ${data.country[0].country_id}`})
+
 }
 
 function fetchImage() {
