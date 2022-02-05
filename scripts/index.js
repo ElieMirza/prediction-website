a67332a73e1380ab244345dfd1ab3a0292e2eeb9
ElieMirza <elie.mirza1987@gmail.com>
2022-02-05 21:47:07 +0200

    nationality is working

--- scripts/index.js
+++ scripts/index.js
@@ -28,20 +28,42 @@ async function fetchNationality(link, userName) {
     await fetch(link + userName)
             .then(response => response.json())
             .then(data => {countries = data.country});
-    console.log(countries);
+
     let gender_para = document.getElementById("nationality");
+    let span = document.createElement('span');
 
-    countries.map(country => {
-      let span = document.createElement('span');
-      console.log(span);
-      span.textContent = country["country_id"];
-      console.log(span);
+    if(countries.length == 0){
+      span.textContent = "No country available";
       gender_para.appendChild(span);
-    });
-
-       // 'country["country_id"].value'
-    // .textContent = `nationality: ${data.country[0].country_id}`})
-
+    }else if (countries.length == 1) {
+      span.textContent = country["country_id"];
+      gender_para.appendChild(span);
+    }else {
+      for(let i = 0; i < countries.length; i++){
+        if(i === (countries.length -1)){
+          console.log("else if");
+          span.textContent = countries[i].country_id;
+          gender_para.appendChild(span);
+        }else {
+          console.log("else else");
+          let span = document.createElement('span');
+          span.textContent = countries[i].country_id + ", ";
+          gender_para.appendChild(span);
+        }
+      }
+    //   countries.forEach((country => {
+    //   if(countries.indexOf((Object.keys(country).find(key => country[key] === country["country_id"]))) === (countries.length -1)){
+    //     console.log("else if");
+    //     span.textContent = country["country_id"];
+    //     gender_para.appendChild(span);
+    //   }else {
+    //     console.log("else else");
+    //     let span = document.createElement('span');
+    //     span.textContent = country["country_id"] + ", ";
+    //     gender_para.appendChild(span);
+    //   }
+    // }))
+    }
 }
 
 function fetchImage() {
