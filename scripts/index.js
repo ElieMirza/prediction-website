a43e3244ea773722a29fa143d7cf69f9b711e206
ElieMirza <elie.mirza1987@gmail.com>
2022-02-05 13:34:04 +0200

    fectch random dog image working

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,26 @@
 let user_input = "";
-let gender = "https://api.genderize.io?name=" + user_input;
-let age = "https://api.agify.io/?name=" + user_input;
-let nationality = "https://api.nationalize.io/?name=" + user_input;
-let random_dog_image = "https://dog.ceo/api/breeds/image/random";
+let gender = "https://api.genderize.io?name=";
+let age = "https://api.agify.io/?name=";
+let nationality = "https://api.nationalize.io/?name=";
+
+fetchImage();
 
 document.getElementById("button").onclick = function(){
   user_input = document.getElementById("input").value;
-  alert(user_input);
+  // fetchJSON(gender, user_input)
+  fetchJSON(age, user_input)
+  // fetchJSON(nationality, user_input)
 }
 
+function fetchJSON(link, userName) {
+  fetch(link + userName)
+    .then(response => response.json())
+    .then(data => alert(data.gender));
+}
 
-
-alert(gender);
-alert(age);
-alert(nationality);
+function fetchImage() {
+  let image = document.getElementById("dog-image");
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => image.src = data.message);
+}
