b6db4b58020092a8a3413c81d12db398496ff01b
ElieMirza <elie.mirza1987@gmail.com>
2022-02-05 12:36:06 +0200

    before starting with apis

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
+let user_input = "";
+let gender = "https://api.genderize.io?name=" + user_input;
+let age = "https://api.agify.io/?name=" + user_input;
+let nationality = "https://api.nationalize.io/?name=" + user_input;
+let random_dog_image = "https://dog.ceo/api/breeds/image/random";
 
+document.getElementById("button").onclick = function(){
+  user_input = document.getElementById("input").value;
+  alert(user_input);
+}
+
+
+
+alert(gender);
+alert(age);
+alert(nationality);
