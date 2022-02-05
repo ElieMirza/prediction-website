884e4ca8b9e2f7d096861bb7ff86a4b79df70030
ElieMirza <elie.mirza1987@gmail.com>
2022-02-05 23:20:41 +0200

    cleaned code and beautified js file

--- scripts/index.js
+++ scripts/index.js
@@ -4,64 +4,62 @@ let age_api = "https://api.agify.io/?name=";
 let nationality_api = "https://api.nationalize.io/?name=";
 let countries = [];
 let game_started = false;
+let nationality_para = document.getElementById("nationality");
 
 fetchImage();
 
-document.getElementById("button").onclick = function(){
+document.getElementById("button").onclick = function() {
   if (!game_started) {
     game_started = true;
     printQueries();
     console.log(game_started + "onclick");
-  }else {
+  } else {
     resetQueries();
     printQueries();
   }
 }
 
 async function fetchGenderAge(link, userName, trait) {
-    await fetch(link + userName)
-            .then(response => response.json())
-            .then(data => document.getElementById(trait).textContent = `${trait}: ${data[trait]}`)
+  await fetch(link + userName)
+    .then(response => response.json())
+    .then(data => {
+      if (data[trait] == null) {
+        document.getElementById(trait).textContent = "no data available";
+      } else {
+        document.getElementById(trait).textContent = `${trait}: ${data[trait]}`;
+      }
+    })
 }
 
 async function fetchNationality(link, userName) {
-    await fetch(link + userName)
-            .then(response => response.json())
-            .then(data => {countries = data.country});
-    printNationality();
-    console.log(game_started + "inside fetchnat")
+  await fetch(link + userName)
+    .then(response => response.json())
+    .then(data => countries = data.country);
+  printNationality();
 }
-console.log(game_started);
 
 function printNationality() {
-    console.log("before ifs");
-    let gender_para = document.getElementById("nationality");
-    let span = document.createElement('span');
+  let span = document.createElement('span');
 
-    if(countries.length == 0){
-      console.log("if");
-      span.textContent = "No country available";
-      gender_para.appendChild(span);
-    }else if (countries.length == 1) {
-      console.log("else if 1");
-      span.textContent = "Nationality: " + country["country_id"];
-      gender_para.appendChild(span);
-    }else {
-      document.getElementById('nationality').textContent = "Nationality: ";
-      console.log("else");
-      for(let i = 0; i < countries.length; i++){
-        if(i === (countries.length -1)){
-          console.log("else if 2");
-          span.textContent = countries[i].country_id;
-          gender_para.appendChild(span);
-        }else {
-          console.log("else else");
-          let span = document.createElement('span');
-          span.textContent = countries[i].country_id + ", ";
-          gender_para.appendChild(span);
-        }
+  if (countries.length == 0) {
+    span.textContent = "No country available";
+    nationality_para.appendChild(span);
+  } else if (countries.length == 1) {
+    span.textContent = "Nationality: " + countries[0].country_id;
+    nationality_para.appendChild(span);
+  } else {
+    document.getElementById('nationality').textContent = "Nationality: ";
+    for (let i = 0; i < countries.length; i++) {
+      if (i === (countries.length - 1)) {
+        span.textContent = countries[i].country_id;
+        nationality_para.appendChild(span);
+      } else {
+        let span = document.createElement('span');
+        span.textContent = countries[i].country_id + ", ";
+        nationality_para.appendChild(span);
       }
     }
+  }
 }
 
 function fetchImage() {
@@ -71,7 +69,7 @@ function fetchImage() {
     .then(data => image.src = data.message);
 }
 
-function printQueries(){
+function printQueries() {
   user_input = document.getElementById("input").value;
   fetchGenderAge(gender_api, user_input, "gender");
   fetchGenderAge(age_api, user_input, "age");
